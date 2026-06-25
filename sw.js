7e768d25f94f7ab947589422a59fd235fc7f59bf
Masamune Endo <mstd0520@gmail.com>
2026-06-25 14:11:31 +0000

    design: タスク詳細アコーディオンヘッダーのグラデーションをフラットなアクセントカラーに変更

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Taskra - Service Worker v38 (stale-while-revalidate)
-const CACHE_NAME = 'taskra-v38';
+// Taskra - Service Worker v39 (stale-while-revalidate)
+const CACHE_NAME = 'taskra-v39';
 const CDN_CACHE  = 'taskra-cdn-v33';
 
 // キャッシュするアセット
